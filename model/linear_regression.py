5560ea1afa63c067834dfcbbd675e20718d22c7f
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-14 21:32:44 +0000

    fix: added doc_strings to the module

--- model/linear_regression.py
+++ model/linear_regression.py
@@ -1,9 +1,14 @@
+"""ML module that implements the Linear Regression Model from scratch using Numpy"""
+
+
 class LinearRegression:
+    """Implementation of Linear Regression Model"""
+
     def __init__(self, lr: float = 0.01, n_iters: int = 1000) -> None:
         pass
 
     def fit(self, X, y):
-        pass
+        """Fit the training data to the model"""
 
     def predict(self, X):
-        pass
+        """Predict the output for the test data"""
