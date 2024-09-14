2a87242bb682f7ee99c3a9312bac87b0a2bc061c
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-14 21:27:04 +0000

    chore: add skeleton code

---
+++ model/LinearRegression.py
@@ -0,0 +1,9 @@
+class LinearRegression:
+    def __init__(self, lr: float = 0.01, n_iters: int = 1000) -> None:
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
